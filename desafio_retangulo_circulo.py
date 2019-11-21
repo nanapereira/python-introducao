6d7f458cbea3a68b19a3ac298ae734b7615486e9
nanapereira <inajara.souza.pereira@gmail.com>
2019-11-21 19:02:23 -0300

    atualizacao

--- desafio_retangulo_circulo.py
+++ desafio_retangulo_circulo.py
@@ -6,22 +6,18 @@ class Retangulo:
         self.ladoy = ladoy
 
     def calcular_area_retangulo(self, ladox, ladoy): 
-        area_retangulo = ladox * ladoy
-        return area_retangulo
+        return ladox * ladoy
 
     def calcular_perimetro_retangulo(sel, ladox, ladoy):
-        perimetro_retangulo = (ladox * 2) + (ladoy + 2)
-        return perimetro_retangulo
+        return (ladox * 2) + (ladoy + 2)
 
 class Circulo:
     def __init__(self, raio):
         self.raio = raio
 
     def calcular_area_circulo(self, raio):
-        area_circulo = (raio ** 2) * math.pi
-        return area_circulo
+        return (raio ** 2) * math.pi
 
     def calcular_perimetro_circulo(self, raio):
-        perimetro_circulo = 2 * math.pi * raio
-        return perimetro_circulo
+        return 2 * math.pi * raio
         
